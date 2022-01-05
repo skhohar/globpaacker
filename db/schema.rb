9e542a1df9f10fdc5918c31be56d806023a65bb0
Ilhem BOUSSAK <boussak_ilhem@yahoo.fr>
2022-01-05 14:58:00 +0100

    change integer to longitude latitude to navigation tab

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_05_110253) do
+ActiveRecord::Schema.define(version: 2022_01_05_135413) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,10 +44,10 @@ ActiveRecord::Schema.define(version: 2022_01_05_110253) do
   end
 
   create_table "navigations", force: :cascade do |t|
-    t.integer "starting_longitude"
-    t.integer "starting_latitude"
-    t.integer "ending_longitude"
-    t.integer "ending_latitude"
+    t.float "starting_longitude"
+    t.float "starting_latitude"
+    t.float "ending_longitude"
+    t.float "ending_latitude"
     t.boolean "done"
     t.date "date"
     t.datetime "created_at", precision: 6, null: false
