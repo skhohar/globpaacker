d46db9d6989923f3e76564f1d2535c63e4c9c2d4
Sarah Khoubbaz-harl <s-kh@live.fr>
2022-01-06 18:01:33 +0100

    in progress

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -25,6 +25,17 @@ const initMapbox = () => {
     });
 
     fitMapToMarkers(map, markers);
+
+    const instructions = document.getElementById('instructions');
+    const steps = data.legs[0].steps;
+
+    let tripInstructions = '';
+    for (const step of steps) {
+      tripInstructions += `<li>${step.maneuver.instruction}</li>`;
+    }
+    instructions.innerHTML = `<p><strong>Trip duration: ${Math.floor(
+      data.duration / 60
+    )} min 🚶 </strong></p><ol>${tripInstructions}</ol>`;
   }
 };
 
