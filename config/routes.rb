475c5cb7f8bebdac6d2d6e68fc3743aac05de550
Sarah Khoubbaz-harl <s-kh@live.fr>
2022-01-07 11:31:45 +0100

    jquery installed

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   get '/geolocalisation', to: 'pages#geolocalisation'
 
-  resources :navigatons, only: %i[show new create] do
+  resources :navigations, only: %i[show new create] do
     resources :places, only: %i[show] do
       member do
         patch :visited, as: 'visited'
