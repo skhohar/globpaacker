e14097b5c1314403561f5561c1b24cd7c2f185ba
Rika Fonseca <fonsecarika@gmail.com>
2022-01-12 15:41:37 +0200

    ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'csv'
-require 'faker'
 require "open-uri"
 
 
