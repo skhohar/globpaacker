68ad00b958ebd718c3c3bad0828d5226b5c7cab7
Sarah Khoubbaz-harl <s-kh@live.fr>
2022-01-13 17:16:04 +0100

    finally working

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -50,11 +50,12 @@ const initMapbox = () => {
         const data = json.routes[0];
 
         const timeContainer = document.querySelector("#time-remaining")
-        const timeAvailable = timeContainer.dataset.timeAvailable
-        const timePlacesPlanned = timeContainer.dataset.timePlacesPlanned
-        const timeWalking = data.duration
+        const timeAvailable = Number(timeContainer.dataset.timeAvailable)
+        const timePlacesPlanned = Number(timeContainer.dataset.timePlacesPlanned)
+        const timeWalking = Number(data.duration.toString().split('.')[0])
+
         const timeRemaining = (timeAvailable - (timePlacesPlanned + timeWalking))/60
-        timeContainer.innerText=`After your exploration, you will still have ${timeRemaining} minutes available at your destination for freestyle!`
+        timeContainer.innerText = `After your exploration, you will still have ${timeRemaining.toString().split('.')[0]} minutes available at your destination for freestyle!`
 
         // console.dir(timeContainer)
         const route = data.geometry.coordinates;
